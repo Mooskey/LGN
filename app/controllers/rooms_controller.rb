736953d0bc6a7b394e5109fea3d5b5de891d38ab
Moshe Weiss <moman5757@gmail.com>
2016-08-16 20:43:32 -0400

    i killed the spooky ghosts

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,6 +1,6 @@
 class RoomsController < ApplicationController
   def show
-    @messages = Message.all.includes(:user)
-    @active = @messages.where(updated_at: (5.minutes.ago..Time.now)).map(&:user).uniq
-  end
+    @messages = Message.all.includes(:user)
+    @active = @messages.where(updated_at: (5.minutes.ago..Time.now)).map(&:user).uniq
+  end
 end
