3164aaf851189e00eca02d94fdc64cae210e429a
Moshe Weiss <moman5757@gmail.com>
2016-08-16 20:37:31 -0400

    active user thanks ed :D

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,13 +1,6 @@
 class RoomsController < ApplicationController
   def show
-    @messages = Message.all
-    # @active = []
-    # msgs = Message.where(updated_at: (5.minutes.ago..Time.now))
-    # if msgs
-    #   msgs.select(:user_id).distinct.each do |recent|
-    #     @active << User.where(id: recent.user_id)
-    #   end
-    # end
-  end
+    @messages = Message.all.includes(:user)
+    @active = @messages.where(updated_at: (5.minutes.ago..Time.now)).map(&:user).uniq
+  end
 end
-# TODO: paginate/limit messages show
